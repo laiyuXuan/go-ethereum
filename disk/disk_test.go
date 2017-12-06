6ed0786441cb99baee47b0e0c354b72dcf2bf90f
laiyuXuan <yuxuan.lai@ijugo.net>
2017-12-06 17:47:50 +0800

    test disk usages

---
+++ disk/disk_test.go
@@ -0,0 +1,33 @@
+package disk
+
+import (
+	"syscall"
+	"testing"
+	"fmt"
+)
+
+func TestDiskUsage(t *testing.T) {
+	DiskUsage("/")
+}
+
+type DiskStatus struct {
+	All  uint64 `json:"all"`
+	Used uint64 `json:"used"`
+	Free uint64 `json:"free"`
+}
+
+// disk usage of path/disk
+func DiskUsage(path string) (disk DiskStatus) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		return
+	}
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+	fmt.Println(disk.All)
+	fmt.Println(disk.Free)
+	fmt.Println(disk.Used)
+	return
+}
